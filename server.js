e7dec8f4ce9570935bbdf2a682f690b349a5c9a1
Dletta <jcduschletta@gmail.com>
2020-04-16 22:36:42 -0700

    added, secret messages

---
+++ server.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const express = require('express');
+const Gun = require('gun');
+const SEA = require("gun/sea");
+
+
+const port = 8081;
+const host = '127.0.0.1';
+
+const app = express();
+app.use(Gun.serve);
+
+const server = app.listen(port, host);
+
+console.log(`server listening on http://${host}:${port}`);
+
+function logIn(msg){
+  console.log(`in msg:${JSON.stringify(msg)}.........`);
+}
+
+function logOut(msg){
+  console.log(`out msg:${JSON.stringify(msg)}.........`);
+}
+
+var gun = Gun({
+  web: server,
+  localStorage: false,
+  radisk: false
+});
+
+gun._.on('in', logIn);
+gun._.on('out', logOut);
+
+function logPeers() {
+  console.log(`Peers: ${Object.keys(gun._.opt.peers).join(', ')}`);
+}
+
+function logData() {
+  console.log(`In Memory: ${JSON.stringify(gun._.graph)}`);
+}
+
+setInterval(logPeers, 5000); //Log peer list every 5 secs
+setInterval(logData, 20000); //Log gun graph every 20 secs
+
+
+const view = path.join(__dirname, 'view/main.html');
+
+app.use(express.static('view'));
+app.get('*', function(_, res) {
+  res.sendFile(view);
+});
+
+
+// Most of this code provided by @thinkingjoules
